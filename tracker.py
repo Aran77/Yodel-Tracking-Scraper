398ca73361910d95d163f95aa835e3b11c47fc01
Aran Smithson <aransmithson@gmail.com>
2023-01-17 12:11:55 +0000

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -1,33 +1,25 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import requests
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 import time
-import array as arr
-import re
 from tinydb import TinyDB, Query, where
 import db as db
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
 from tkinter import filedialog as fd
-import json
+from tkinter import messagebox
 
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
-DRIVER_PATH = 'c:\python\chrome driver\chromedriver.exe'
-driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+
 year = str(datetime.today().year)
 
 def on_quit():
-    driver.quit()
     window.destroy()
 
 def update_status(text):
     status_text.set(text)
+
+
 
 window = tk.Tk()
 window.title("YodelTracker")
@@ -43,6 +35,12 @@ height = int(screen_height * 0.9)
 
 # Set the dimensions of the window
 window.geometry(f"{width}x{height}")
+
+def tree_click_handler(event):
+    cur_item = data_table.item(data_table.focus())
+    col = data_table.identify_column(event.x)
+    window.clipboard_clear()
+    window.clipboard_append('https://www.yodel.co.uk/tracking/'+ cur_item['values'][1] + "/"+ cur_item['values'][7])
 
 def date_diff(date1, date2):
     d1 = datetime.strptime(date1, "%Y-%m-%d")
@@ -61,86 +59,53 @@ def populate_data():
         if inner_list[9]:
             days = date_diff(l[2], l[9])
             l.append(days)
-        print(i, l)
         data_table.insert('', 'end', text=i, values=l)
+    data_table.bind('<ButtonRelease-1>', tree_click_handler)
     update_status(str(len(d)) + " Consignments Loaded.")
 
-def fireitup(tn,pc):
-    update_status("Processing..." + tn)
+def delete_item():
+    selected_item = data_table.selection()[0] # get the selected item
+    tmptn = data_table.item(selected_item, "values")[1]
+    print(tmptn)
+    confirm = messagebox.askyesno("Confirm","Are you sure you want to delete this item?")
+    if confirm:
+        data_table.delete(selected_item) # delete the selected item
+        c,conn = db.connect_to_db()
+        db.updatedb(c, conn, "DTD", "NA",tmptn)
+    else:
+        return
+
+def scrapethePage(tn, pc):
     url = 'https://www.yodel.co.uk/tracking'
-    bigurl = url +'/' + tn + '/' +pc +'?headless=true'
-    driver.get(bigurl)
-    #driver.implicitly_wait(0.0)
-                                              
-    elements = driver.find_elements(By.XPATH,"/html/body/div/div[3]/div[3]/div/div[1]/div[2]/div[1]/div/div/div[2]")
-    elements1 = driver.find_elements(By.XPATH,'/html/body/div[1]/div[3]/div[3]/div/div/div[1]/div[1]/div[2]/div[1]/div/div/div[2]')
-    elements2 = driver.find_elements(By.XPATH, '/html/body/div/div[3]/div[3]/div/div[1]/div[2]/div[1]/div/div/div/div[2]')
-    elemets3 = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[1]/div/div/div[2]")
-    if elements:
-        if elements[0].is_displayed():
-            tmpStatus = elements[0].get_attribute('innerHTML').strip()
-        #    print('elements '+ tmpStatus)
-            if tmpStatus == "We have updated your parcel with your neighbour preferences.":
-                status = "Neightbour preference updated"
-                dd = 'Expected delivery: ' + driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus == "Your parcel has been delivered to a safe place":
-                status = "Delivered, Safe Place" + ": " + driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[3]/div[2]').get_attribute('innerHTML').strip()
-                dd = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[1]/div[2]').get_attribute('innerHTML').strip() + ' ' + driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus == "Your parcel has arrived at the local delivery depot" or tmpStatus == "Your parcel has arrived at your local depot" or tmpStatus[:51] == 'Your parcel has arrived at the local delivery depot' or tmpStatus[:43] == "Your parcel is at your local delivery depot":
-                status = "Local Depot"
-                dd = 'Expected Delivery: ' +driver.find_element(By.XPATH, '/html/body/div/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus == "Your parcel has been delivered" or tmpStatus == "Your parcel has been delivered through your letterbox":
-                status = "Delivered"
-                dd = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[1]/div[2]').get_attribute('innerHTML').strip() +' '+ driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus == 'Your parcel has been delivered to your neighbour':
-                status = "Delivered to Neighbour at " + driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[3]/div[2]').get_attribute('innerHTML').strip()
-                dd = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[1]/div[2]').get_attribute('innerHTML').strip() + ' '+ driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus[:15] == "We're expecting":
-                status = "Not Dispatched"
-                dd = "n/a"
-            elif tmpStatus == "Your parcel is on the way but the driver has experienced a short delay. Please check back for updates":
-                status = "Short Delay"
-                dd = driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus[:31] == "Your parcel is out for delivery":
-                status = tmpStatus
-                dd = driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus[:31] == "Your parcel is in transit":
-                status = tmpStatus
-                dd = driver.find_element(By.XPATH,'/html/body/div/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus == "Your parcel is at our national hub":
-                status = tmpStatus
-                dd = driver.find_element(By.XPATH,'/html/body/div/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
-    elif elements1:
-        if elements1[0].is_displayed():
-            tmpStatus = elements1[0].get_attribute('innerHTML').strip()
-        #    print('Element1: '+ tmpStatus)
-            if tmpStatus[:31] == "Your parcel is out for delivery":
-                status = tmpStatus
-                dd = driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus == 'Your parcel is being loaded. Check back for updates shortly.':
-                status = "Being loaded, check back soon"
-                dd = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div/div[1]/div[1]/div[2]/div[1]/div/div/div[2]').get_attribute('innerHTML').strip()
-            elif tmpStatus[:11] == "Your driver":
-                status = "Out for delivery today"
-                dd = "Today"
-    elif elements2:
-        if elements2[0].is_displayed():
-            tmpStatus = elements2[0].get_attribute('innerHTML').strip()
-            if tmpStatus[:31] == "Your parcel is out for delivery":
-                status = tmpStatus
-                dd = driver.find_element(By.XPATH,'/html/body/div/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
-    elif elements3:
-        print("element3")
-        if elements2[0].is_displayed():
-            tmpStatus = elements2[0].get_attribute('innerHTML').strip()
-            if tmpStatus == "We need some more information about your address. Please chat with us.":
-                status = tmpStatus
-                dd = driver.find_element(Bt.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[2]/div/div/div[2]').get_attribute('innerHTML').strip()
+    bigurl = url +'/' + tn + '/' +pc +'?headless=true'   
+    response = requests.get(bigurl)
+    soup = BeautifulSoup(response.text, 'html.parser')  
+    if not soup.find("yodel-hero_tracking-form"):        
+        target_divs = soup.find_all("div", class_="parcel-current-status-message")
+        for div in target_divs:
+            content = div.get_text()
+            status = content.strip()
+            if status[:30] == "Your parcel has been delivered":
+                tmptarget_divs = soup.find_all("div", class_="parcel-status-delivered-date")  
+            else:
+                tmptarget_divs = soup.find_all("div", class_="parcel-status-value")  
+            for tmpdiv in tmptarget_divs:
+                content1 = tmpdiv.get_text()
+                dd = content1.strip()
+            if status[:30] == "Your parcel has been delivered":
+                status = "Delivered"  
+            print(tn)          
+        return status, dd
     else:
         status = "Error"
         dd = "Error"
-    print(tn + " "  + status)
-    return status.strip(), dd.strip()
+        return status, dd
+
+ 
+        
+
+
+
 
 def convert_written_date(written_date):
     day_of_week, day, month, year = written_date.split()
@@ -161,21 +126,27 @@ def refreshData():
         tn = d[1]
         dd = d[2]
         pc = d[7]
+        formatted_date = ""
         failures = []
         if pc != "Redacted":
-            status, delivered = fireitup(tn,pc)
-            if status[:3] == 'Del':
-                formatted_date = convert_written_date(delivered[:-6] +' '+ year)
+            status, delivered = scrapethePage(tn,pc)
+            print("********** date: "+ delivered)
+            if status[:30] == 'Your parcel has been delivered':                
+                formatted_date = convert_written_date(delivered +' '+ year)
             else:
-                formatted_date = datetime.strftime(datetime.now(), "%Y-%m-%d")
-                db.update_db(c,conn, status, formatted_date, tn)
+                if delivered != "Unavailable at this time" and delivered != "Today" and ":" not in delivered and "stop" not in status:
+                    formatted_date = convert_written_date(delivered +' '+ year)                
+                else:
+                    formatted_date = datetime.strftime(datetime.now(), "%Y-%m-%d")
+            db.update_db(c,conn, status, formatted_date, tn)
         else:
             failures.append(tn)
     if failures:
         db.message_box("Refresh Complete", "Refresh Complete with some errors\n" + failures)
     else:
-        update_status("Refresh Complete, " + len(datatable) + "consignments refreshed.")
+        update_status("Refresh Complete")
     populate_data()
+
 
 def importData():
     c,conn = db.connect_to_db()
@@ -187,7 +158,7 @@ def importData():
         pc = x['pc'].replace(" ","")
         if pc != "Redacted":
             update_status("Querying Yodel..."+tn)
-            status, delivered = fireitup(tn,pc)
+            status, delivered = scrapethePage(tn,pc)
             if status[:3] == 'Del':
                 formatted_date = convert_written_date(delivered[:-6] +' '+ year)
             else:
@@ -203,14 +174,16 @@ def importData():
         print(failures)
     populate_data()
 
+busy_frame = ttk.Frame(window, width=50, height=50, relief='sunken')
+busy_frame.place(relx=0.5, rely=0.5, anchor='center')
 control_bar = tk.Frame(window)
 control_bar.pack(side='top', fill='x')
 open_new_data_button = tk.Button(control_bar, text="Import New Consignments", command=getNewConsignments)
 open_new_data_button.pack(side='left')
 get_statuses_button = tk.Button(control_bar, text="Refresh", command=refreshData)
 get_statuses_button.pack(side='left')
-report_button = tk.Button(control_bar, text="Report")
-report_button.pack(side='left')
+delete_button = tk.Button(control_bar, text="Delete", command=delete_item, bg="red",fg="white")
+delete_button.pack(side='left')
 quit_button = tk.Button(control_bar, text="Quit", command=on_quit)
 quit_button.pack(side='right')
 data_table_frame = tk.Frame(window)
@@ -226,14 +199,13 @@ status = tk.Label(data_table_frame, textvariable=status_text, text="Ready", bd=1
 status.pack(side=tk.BOTTOM, fill=tk.X)
 
 headings = ('ID','Tracking_Number', 'Dispatch_Date','Order_ID','Ex_Order_ID','Source','Service','Postcode','Status','Expected','Days')
+
 data_table['columns'] = ('ID','Tracking_Number', 'Dispatch_Date','Order_ID','Ex_Order_ID','Source','Service','Postcode','Status','Expected','Days')
 for i in headings:
     data_table.heading(i, text=i)
 for i in headings:
-    data_table.column(i, stretch=tk.YES, width=100)
+    data_table.column(i, stretch=tk.YES, width=100)    
 data_table.column("#0", width=0)
-for i in headings:
-    data_table.heading(i, text=i)
 
 populate_data()
 window.mainloop()
