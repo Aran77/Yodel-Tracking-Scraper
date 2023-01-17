055c334d91c3ae8189ed2389f687985213ab5cba
Aran Smithson <aransmithson@gmail.com>
2023-01-17 12:19:54 +0000

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -3,28 +3,28 @@ import requests
 import pandas as pd
 from datetime import datetime
 import time
-from tinydb import TinyDB, Query, where
 import db as db
 import tkinter as tk
 from tkinter import ttk
-from tkinter import filedialog as fd
 from tkinter import messagebox
 
-
+#get the year
 year = str(datetime.today().year)
 
+#quit program function
 def on_quit():
     window.destroy()
 
+#updates the status bar with user updates
 def update_status(text):
     status_text.set(text)
 
-
-
+#create main Tkinter window
 window = tk.Tk()
 window.title("YodelTracker")
 status_text = tk.StringVar()
 status_text.set("Ready")
+
 # Get the screen width and height
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
@@ -36,22 +36,24 @@ height = int(screen_height * 0.9)
 # Set the dimensions of the window
 window.geometry(f"{width}x{height}")
 
+#handles the clicking of rows to get the tracking URL
 def tree_click_handler(event):
     cur_item = data_table.item(data_table.focus())
     col = data_table.identify_column(event.x)
     window.clipboard_clear()
     window.clipboard_append('https://www.yodel.co.uk/tracking/'+ cur_item['values'][1] + "/"+ cur_item['values'][7])
 
+#calculate difference in dispatch to cureent day/delivery day
 def date_diff(date1, date2):
     d1 = datetime.strptime(date1, "%Y-%m-%d")
     d2 = datetime.strptime(date2, "%Y-%m-%d")
     return abs((d2 - d1).days)
 
+#Our main refresh function which clears the datatable and reloads from DB
 def populate_data():
     update_status("Loading Data...")
     c,conn = db.connect_to_db()
     d = db.open_pending_data(c)
-    
     for row in data_table.get_children():
         data_table.delete(row)
     for i, inner_list in enumerate(d):
@@ -63,6 +65,7 @@ def populate_data():
     data_table.bind('<ButtonRelease-1>', tree_click_handler)
     update_status(str(len(d)) + " Consignments Loaded.")
 
+#Remove an item from data table
 def delete_item():
     selected_item = data_table.selection()[0] # get the selected item
     tmptn = data_table.item(selected_item, "values")[1]
@@ -75,6 +78,7 @@ def delete_item():
     else:
         return
 
+#Main scraping function
 def scrapethePage(tn, pc):
     url = 'https://www.yodel.co.uk/tracking'
     bigurl = url +'/' + tn + '/' +pc +'?headless=true'   
@@ -101,23 +105,20 @@ def scrapethePage(tn, pc):
         dd = "Error"
         return status, dd
 
- 
-        
-
-
-
-
+#convert written data to yyy/mm/ddd
 def convert_written_date(written_date):
     day_of_week, day, month, year = written_date.split()
     month_number = datetime.strptime(month, '%B').month
     day = day[:-2]
     return f"{year}-{month_number}-{day}"
 
+#load consignments in from FTP and then refresh the table
 def getNewConsignments():
     #db.open_yodel_file()
     db.importfromFTP()
     populate_data()
 
+#Main function to call API
 def refreshData():
     update_status("Updating Statuses...")
     c,conn = db.connect_to_db()
@@ -147,7 +148,7 @@ def refreshData():
         update_status("Refresh Complete")
     populate_data()
 
-
+#defunct, this was our initial method to populate DB
 def importData():
     c,conn = db.connect_to_db()
     d = importfromFTP()
@@ -174,10 +175,11 @@ def importData():
         print(failures)
     populate_data()
 
-busy_frame = ttk.Frame(window, width=50, height=50, relief='sunken')
-busy_frame.place(relx=0.5, rely=0.5, anchor='center')
+#Create Tkinter Layout
+#Add a horizontal top bar to host our buttons
 control_bar = tk.Frame(window)
 control_bar.pack(side='top', fill='x')
+#buttons
 open_new_data_button = tk.Button(control_bar, text="Import New Consignments", command=getNewConsignments)
 open_new_data_button.pack(side='left')
 get_statuses_button = tk.Button(control_bar, text="Refresh", command=refreshData)
@@ -186,26 +188,29 @@ delete_button = tk.Button(control_bar, text="Delete", command=delete_item, bg="r
 delete_button.pack(side='left')
 quit_button = tk.Button(control_bar, text="Quit", command=on_quit)
 quit_button.pack(side='right')
+#create data table frame
 data_table_frame = tk.Frame(window)
 data_table_frame.pack(side='bottom', fill='both', expand=True)
 data_table = ttk.Treeview(data_table_frame)
+#add a veritacal scrollbar
 treeScroll = ttk.Scrollbar(data_table)
 treeScroll.configure(command=data_table.yview)
 data_table.configure(yscrollcommand=treeScroll.set)
 treeScroll.pack(side='right', fill='both')
 data_table.pack(side='top', fill='both', expand=True)
-
 status = tk.Label(data_table_frame, textvariable=status_text, text="Ready", bd=1, relief=tk.SUNKEN, anchor=tk.W)
 status.pack(side=tk.BOTTOM, fill=tk.X)
-
+#create headings for data_table
 headings = ('ID','Tracking_Number', 'Dispatch_Date','Order_ID','Ex_Order_ID','Source','Service','Postcode','Status','Expected','Days')
-
 data_table['columns'] = ('ID','Tracking_Number', 'Dispatch_Date','Order_ID','Ex_Order_ID','Source','Service','Postcode','Status','Expected','Days')
+#parse headings into datatable
 for i in headings:
     data_table.heading(i, text=i)
 for i in headings:
     data_table.column(i, stretch=tk.YES, width=100)    
 data_table.column("#0", width=0)
 
+#Populate Data is our main Fresh function
 populate_data()
+
 window.mainloop()
